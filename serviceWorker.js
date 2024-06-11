236b8a0dddce0af86bb9629eba49f132a9ed4e8d
hinacoppy <kawakami@js4.so-net.ne.jp>
2024-06-11 18:11:05 +0900

    update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,7 +2,7 @@
 // (参考) https://developer.mozilla.org/ja/docs/Web/Progressive_web_apps/Offline_Service_workers
 'use strict';
 
-const cacheName = 'bgscore-v20230210';
+const cacheName = 'bgscore-v20240611';
 const ORIGIN = (location.hostname == 'localhost') ? '' : location.protocol + '//' + location.hostname;
 
 const contentToCache = [
@@ -18,8 +18,7 @@ const contentToCache = [
   ORIGIN + '/bgscoreapp/js/bgflipcard_class.js',
   ORIGIN + '/bgscoreapp/js/swipetracker_class.js',
   ORIGIN + '/bgscoreapp/js/bgscoreapp_class.js',
-  ORIGIN + '/js/jquery-3.6.1.min.js',
-  ORIGIN + '/js/start-serviceWorker.js',
+  ORIGIN + '/js/jquery-3.7.1.min.js',
 ];
 
 self.addEventListener('install', (e) => {
@@ -28,13 +27,16 @@ self.addEventListener('install', (e) => {
       return cache.addAll(contentToCache);
     })
   );
+  self.skipWaiting();
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((r) => {
       return r || fetch(e.request).then((response) => {
         return caches.open(cacheName).then((cache) => {
-          cache.put(e.request, response.clone());
+          if (e.request.url.startsWith('http')) { //ignore chrome-extention: request (refuse error msg)
+            cache.put(e.request, response.clone());
+          }
           return response;
         });
       });
